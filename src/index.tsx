dc28faa966a5ecb7d1a1cdbad6c42493ae685af8
Ivanitsky-Oleksii <goleecindj@gmail.com>
2023-01-26 16:02:22 +0200

    add list

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const h1 = React.createElement("h1",{id: "title"}, "Hello React")
+// const h1 = React.createElement("h1",{id: "title"}, "Hello React")
 
-const list = React.createElement(
-  "ul",
-  null,
-  React.createElement("li", null, "list item 1"),
-  React.createElement("li", null, "list item 2"),
-  React.createElement("li", null, "list item 3"),
-  React.createElement("li", null, "list item 4")
-)
+// jsx
+const h1 = <h1 id="title">Hello React.js</h1>
+
+const list = <ul>
+  <li>list item 1</li>
+  <li>list item 4</li>
+  <li>list item 5</li>
+</ul>
+
+
+// const list = React.createElement(
+//   "ul",
+//   null,
+//   React.createElement("li", null, "list item 1"),
+//   React.createElement("li", null, "list item 2"),
+//   React.createElement("li", null, "list item 3"),
+//   React.createElement("li", null, "list item 4")
+// )
 
 const content = (
   <div>
