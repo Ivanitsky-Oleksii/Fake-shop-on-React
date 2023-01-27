6a70268ea0693f0cb19b54bfb76272cf0a448a4e
Ivanitsky-Oleksii <goleecindj@gmail.com>
2023-01-27 14:41:59 +0200

    add style in object for text1

--- src/Content.tsx
+++ src/Content.tsx
@@ -7,7 +7,12 @@ year : number
 export const Content = (props: Props) => {
 return (
 <>
-<p>{props.text1}</p>
+<p style = {{
+    color: "white",
+    background: "green",
+    padding: "15px"
+}}>
+    {props.text1}</p>
 <p>{props.text2}</p>
 <div>Year: {props.year}</div>
 </>
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom/client'
 import {Content} from './Content'
 import Title from './Title'
 
-
 function App(){
   return (
   <>
   <Title text="React"/>
   <Title text="TS"/>
-  <Content text1='Hello world 1' text2="Hello world 2" year={2023}/>
+  <Content text1='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio qui consequuntur sed debitis error aut praesentium quo nihil architecto tempore natus suscipit, inventore, incidunt nam dolorem maiores consequatur mollitia reiciendis?'
+  text2="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio qui consequuntur sed debitis error aut praesentium quo nihil architecto tempore natus suscipit, inventore, incidunt nam dolorem maiores consequatur mollitia reiciendis?" year={2023}/>
 </>)
 }
 
