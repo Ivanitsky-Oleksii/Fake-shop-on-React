616e56cfd06aad32a6e91251af02110ac6ec928d
Ivanitsky-Oleksii <goleecindj@gmail.com>
2023-01-27 15:25:59 +0200

    add baseUrl in tsconfig.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,10 @@
 import { type } from '@testing-library/user-event/dist/type'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Content} from './Content'
-import Title from './Title'
 
-function App(){
-  return (
-  <>
-  <Title text="React"/>
-  <Title text="TS"/>
-  <Content text1='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio qui consequuntur sed debitis error aut praesentium quo nihil architecto tempore natus suscipit, inventore, incidunt nam dolorem maiores consequatur mollitia reiciendis?'
-  text2="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio qui consequuntur sed debitis error aut praesentium quo nihil architecto tempore natus suscipit, inventore, incidunt nam dolorem maiores consequatur mollitia reiciendis?" year={2023}/>
-</>)
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<React.StrictMode>
-<App/>
+
   </React.StrictMode>)
