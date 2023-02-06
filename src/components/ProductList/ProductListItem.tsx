6124770f1d1a3c3a1a960f9aa778b6d391b3f3a7
Ivanitsky-Oleksii <goleecindj@gmail.com>
2023-02-06 12:07:31 +0200

    add props destruction

--- src/components/ProductList/ProductListItem.tsx
+++ src/components/ProductList/ProductListItem.tsx
@@ -8,15 +8,21 @@ type Props = {
     capacity: string
     price:number
 }
-const ProductListItem = (props: Props) => {
+const ProductListItem = ({
+    title,
+    descriptions,
+    type,
+    capacity,
+    price,
+}: Props) => {
 return (
     <Card variant="outlined" className="product">
         <CardContent>
-            <div className="product-title">{props.title}</div>
-            <div className="product-desc">{props.descriptions}</div>
-            <div className="product-features">{props.type}</div>
-            <div className="product-features">{props.capacity}</div>
-            <div className="product-price">{props.price}</div>
+            <div className="product-title">{title}</div>
+            <div className="product-desc">{descriptions}</div>
+            <div className="product-features">{type}</div>
+            <div className="product-features">{capacity}</div>
+            <div className="product-price">{price}$</div>
         </CardContent>
         <CardActions className="product-btn-wrap">
             <Button variant="outlined">Add to cart</Button>
