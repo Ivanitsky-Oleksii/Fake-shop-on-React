f4c6214a1a69e15e6845ff6452b00fc784f9aed3
Ivanitsky-Oleksii <goleecindj@gmail.com>
2023-02-06 11:43:21 +0200

    add ProductListItem and style product

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,7 +1,15 @@
+import Home from 'pages/Home/Home'
+import { Container } from '@mui/system'
+
 type Props = {}
 const Main = (props: Props) => {
 return (
-    <div>Main</div>
+    <Container 
+    sx={{
+        padding: '60px 0',
+    }}>
+    <Home />
+    </Container>
 )
 }
 export default Main
