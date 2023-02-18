6bf3b7d5481fc118ec653ee32c211c7dbe0c579a
Ivanitskyi Oleksii <goleecindj@gmail.com>
2023-02-18 11:56:38 +0200

    add route, add cart page

--- src/container/app/App.tsx
+++ src/container/app/App.tsx
@@ -1,18 +1,21 @@
-import Main from "container/Main/Main"
 import Header from "container/Header/Header"
 import Footer from "container/Footer/Footer"
 import CssBaseline from '@mui/material/CssBaseline';
 import { StyledEngineProvider } from '@mui/material/styles';
 import { useState } from "react";
+import {Route,Routes} from 'react-router-dom'
+import {Container} from '@mui/system'
+import Home from "pages/Home/Home";
+import CartPage from "pages/Cart/CartPage";
 
-type Props = {}
+type Props={}
 
 type ProductsInCart = {
 [id:number]:number}
 
 const App = (props: Props) => {
 const [productsInCart, setProductsInCart] = useState<ProductsInCart>({
-1:5,
+// 1:5,
 2:5,
 })
 
@@ -26,7 +29,14 @@ const addProductToCart = (id:number,count:number) => {
     <StyledEngineProvider injectFirst>
     <CssBaseline/>
     <Header productsInCart={productsInCart} />
-    <Main addProductToCart={addProductToCart}/>
+    <Container  sx={{
+        padding: '60px 0',
+    }}>
+    <Routes>
+      <Route path='/' element={<Home addProductToCart={addProductToCart} />}/>
+      <Route path="cart" element={<CartPage/>}/>
+    </Routes>
+    </Container>
     <Footer />
     </StyledEngineProvider>
   )
