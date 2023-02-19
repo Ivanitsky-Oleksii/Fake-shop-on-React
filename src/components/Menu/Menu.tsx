21fe6b7532f7e044082f52e0bc200b1264b140f6
Ivanitskyi Oleksii <goleecindj@gmail.com>
2023-02-19 16:07:54 +0200

    style menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,15 +1,21 @@
 import Button from '@mui/material/Button'
 import {Link} from 'react-router-dom'
+import './Menu.scss'
 
-type Props = {}
+type Props = {
+}
+const linkStyle = {textDecoration: 'none', 
+color:"white"
+}
+
 const Menu = (props: Props) => {
     return (
         <>
-    <Button color="inherit"><Link to="/">Home</Link></Button>
+    <Button color="inherit"><Link style={linkStyle} to="/">Home</Link></Button>
     <Button color="inherit">About</Button>
     <Button color="inherit">Payment</Button>
     <Button color="inherit">Shipping</Button>
-    <Button color="inherit"><Link to="/cart">Cart</Link></Button>
+    <Button color="inherit"><Link className='cart-btn' to="/cart">Cart</Link></Button>
     </>
     )
 }
