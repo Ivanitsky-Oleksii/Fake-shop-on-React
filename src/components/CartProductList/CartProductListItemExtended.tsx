08f00e4e99d79c75c330b2b9de27f3cda786fe5a
Ivanitskyi Oleksii <goleecindj@gmail.com>
2023-02-22 12:52:09 +0200

    add CartProductListItem and Extended

---
+++ src/components/CartProductList/CartProductListItemExtended.tsx
@@ -0,0 +1,34 @@
+import { Grid, Card, CardContent, Button,CardActions } from "@mui/material"
+import { Product } from "utils/productsArray"
+import DeleteIcon from '@mui/icons-material/Delete'
+
+
+type Props = {
+    productCount:number
+    product:Product
+}
+
+const CartProductListItemExtended = ({product,productCount}:Props) => {
+    return(
+        <Grid item xs={12} sm={4}>
+<Card>
+    <CardContent>
+        <div className="product-image">
+            <img src={product.image} alt=""/>
+        </div>
+        <div>{product.title}</div>
+        <p>Price for one item: {product.price}</p>
+        <p>Count:{productCount}</p>
+    </CardContent>
+    <CardActions>
+    <Button variant="outlined">
+        <DeleteIcon/>
+    </Button>
+    </CardActions>
+
+</Card>
+
+        </Grid>
+    )
+}
+export default CartProductListItemExtended
--- src/pages/Cart/CartPage.tsx
+++ src/pages/Cart/CartPage.tsx
@@ -1,6 +1,8 @@
 import CartTotal from "components/CartTotal/CartTotal"
 import productsArray, { getProductsObject, Product } from "utils/productsArray"
+import { Typography,Grid } from "@mui/material"
 import CartProductList from "components/CartProductList/CartProductList"
+import CartProductListItemExtended from "components/CartProductList/CartProductListItemExtended"
 
 type Props = {
     productsInCart:{
@@ -13,7 +15,13 @@ productsObject?:{
 const CartPage = ({productsInCart,productsObject = getProductsObject(productsArray)}: Props) => {
     return(
 <div>
-        <CartProductList productsInCart={productsInCart}/>
+<Typography variant="h4" component="h1" sx={{
+    marginBottom:"30px"
+}}>     Cart
+        </Typography>  
+        <Grid container spacing={4}>
+        <CartProductList productsInCart={productsInCart} CartItem={CartProductListItemExtended}/>
+        </Grid> 
         <CartTotal productsInCart={productsInCart}/>
 
 </div>
