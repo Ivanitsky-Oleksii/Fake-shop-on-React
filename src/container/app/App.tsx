4e5f20f335441e3b9f3da71e0795065e9c2bf26b
Ivanitskyi Oleksii <goleecindj@gmail.com>
2023-03-06 16:27:13 +0200

    add first task

--- src/container/app/App.tsx
+++ src/container/app/App.tsx
@@ -1,39 +1,13 @@
-import Header from "container/Header/Header"
-import Footer from "container/Footer/Footer"
-import CssBaseline from '@mui/material/CssBaseline';
-import { StyledEngineProvider } from '@mui/material/styles';
-import { useEffect, useState } from "react";
-import {Route,Routes} from 'react-router-dom'
-import {Container} from '@mui/system'
 import Home from "pages/Home/Home";
-import CartPage from "pages/Cart/CartPage";
-import {omit} from 'lodash'
-import CheckoutPage from "pages/Checkout/CheckoutPage";
-import { useAppDispatch } from "redux/hook";
-import { fetchProducts } from "redux/productsReducer";
 
 type Props={}
 
-const App = (props: Props) => {
-const dispatch = useAppDispatch()
-useEffect(()=>{
-  dispatch(fetchProducts())
-})
+
+const App = (props:Props) =>{
   return (
-    <StyledEngineProvider injectFirst>
-    <CssBaseline/>
-    <Header  />
-    <Container  sx={{
-        padding: '60px 0',
-    }}>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='checkout' element={<CheckoutPage/>}/>
-      <Route path="cart" element={<CartPage/>}/>
-    </Routes>
-    </Container>
-    <Footer />
-    </StyledEngineProvider>
-  )
+<>
+<Home/>
+</>
+)
 }
 export default App
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,31 @@
-import ProductList from "components/ProductList/ProductList"
-import Reviews from "components/Reviews/Reviews"
+import {Component} from 'react' 
+import {Button} from '@mui/material'
+import React, { useState, useEffect } from 'react';
+import { type } from 'os';
 
-const Home = () => {
+
+const Home  = 
+function Count1() {
+const [count1, setCount1] = useState(1);
+const [count2, setCount2] = useState(6);
+const [count3, setCount3] = useState(0);
 return (
+<>
+<h1>Перше завдання</h1>
     <div>
-<ProductList/>
-<Reviews/>
-
-
+    <button onClick={() => setCount1(count1 + 1)}>
+        chenge count ({count1})
+    </button>
+    <button onClick={() => setCount2(count2 + 1)}>
+        chenge count ({count2})
+    </button>
+    <button onClick={() => setCount3(count3 + 1)}>
+        chenge count ({count3})
+    </button>
     </div>
-)
+</>
+);
 }
+
+
 export default Home
